43abe0f88110dbc0caf6d3c12b283134477cd9e4
Taku Kawasaki <takukawasaki@realm00.com>
2016-05-19 16:18:08 +0900

    first commit

---
+++ pollServer.c
@@ -0,0 +1,17 @@
+#include "netlib.h"
+
+
+int
+main(int argc, char *argv[])
+{
+     int listenfd;
+     listenfd = Makesocket(argv[1],argv[2]);
+     err_print("server ready\n");
+     
+
+     poll_accept_loop(listenfd);
+
+     Close(listenfd);
+     return 0;
+}
+
